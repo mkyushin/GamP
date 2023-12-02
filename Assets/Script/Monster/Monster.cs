5afe0ea87a470812dcc85c5a0027dc6ff03ab1c6
taehyeoon <tedxpem@naver.com>
2023-12-03 01:56:05 +0900

    Feat: 몬스터가 플레이어 따라오는 기능 구현
    map의 ground에 static 설정 후, Nav Mesh Surface bake 필수
    Add AI_Navigation package

--- Assets/Script/Monster/Monster.cs
+++ Assets/Script/Monster/Monster.cs
@@ -1,18 +1,109 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Monster : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Stat
+    public float health;
+    public float speed;
+    // if player is in this range, monster detect player, and chase player
+    public float detectPlayerRange;
+    public float attackRange;
+    public float attackSpeed;
+
+    // Const Value
+    private const float DetectPlayerTick = 0.2f;
+
+    // Player Position
+    private Transform _playerTransform;
+     
+    // Components
+    private Animator _anim;
+    private NavMeshAgent _nav;
+    
+    // State
+    private bool _isDetectPlayer;
+    private bool _isAttacking = false;
+    private bool _isRunning = false;
+
+    private void Awake()
     {
+        // Get Player Position
+        _playerTransform = GameObject.FindWithTag("Player").transform;
         
+        // Get Components
+        _anim = GetComponent<Animator>();
+        _nav = GetComponent<NavMeshAgent>();
+        
+        // Set Stat
+        _nav.speed = speed;
+        
+        // Start Coroutine
+        StartCoroutine(IsPlayerInsideOfDetectRange());
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private void Update()
     {
-        
+        if (_isDetectPlayer)
+        {
+            _nav.SetDestination(_playerTransform.position);
+        }
     }
+
+    private IEnumerator IsPlayerInsideOfDetectRange()
+    {
+        while (true)
+        {
+            // If distance between monster and player is less than detectPlayerRange,
+            // monster detect player
+            // monster will follow player until monster is dead or player is out of detectPlayerRange
+            float distance = Vector3.Distance(transform.position, _playerTransform.position);
+            if (distance < detectPlayerRange)
+            {
+                _isDetectPlayer = true;
+
+                StartCoroutine(IsPlayerOutsideOfDetectRange());
+                yield break;
+            }
+            yield return new WaitForSeconds(DetectPlayerTick);
+        }
+    }
+
+    private IEnumerator IsPlayerOutsideOfDetectRange()
+    {
+        while (true)
+        {
+            // If distance between monster and player is greater than detectPlayerRange,
+            // monster stop detect player
+            float distance = Vector3.Distance(transform.position, _playerTransform.position);
+            if (distance > detectPlayerRange)
+            {
+                _isDetectPlayer = false;
+                
+                // Stop NavMeshAgent
+                _nav.SetDestination(transform.position);
+
+                StartCoroutine(IsPlayerInsideOfDetectRange());
+                yield break;
+            }
+            yield return new WaitForSeconds(DetectPlayerTick);
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        // Show Detect Player Range
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, detectPlayerRange);
+        
+        // Show Attack Range
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+    
+    
 }
