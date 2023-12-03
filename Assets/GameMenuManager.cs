8b0fa92fa9d52607a2034e823f6609fe817c1abc
mkshin <alsrb7637@naver.com>
2023-12-03 22:44:59 +0900

    Add feature_vr canvas on off available
    왼쪽 컨트롤러 메뉴버튼 눌러서 vrcanvas 끄고 켤수 있음

--- Assets/GameMenuManager.cs
+++ Assets/GameMenuManager.cs
@@ -1,41 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CommonUsages = UnityEngine.XR.CommonUsages;
 using UnityEngine.InputSystem;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class MenuButtonToggle : MonoBehaviour
+
+public class GameMenuManager : MonoBehaviour
 {
-    public GameObject menu; // 켜고 끌 메뉴를 연결할 변수
-    public Transform head;
-    public float spawnDistance;
+    public XRController leftController;
+    public GameObject cubeObject;
 
-
-
-
-    public InputActionReference showButton;
-
-
-
-    void Update()
+    private void Start()
     {
-
-        
-        // 왼쪽 컨트롤러의 메뉴 버튼이 눌리면
-        if (showButton.action.triggered == true) 
+        if (leftController == null)
         {
-            Debug.Log("pressed");  
-            menu.SetActive(!menu.activeSelf);
-            
-            menu.transform.position = head.position+ new Vector3(head.forward.x, 0, head.forward.z).normalized*spawnDistance;
-
-
-
+            Debug.LogError("Left controller is not assigned!");
         }
-
-        menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
-        menu.transform.forward *= -1;
-
-
+        else
+        {
+            // 등록된 메뉴 버튼 입력 액션에 대한 콜백 함수를 설정합니다.
+            leftController.inputDevice.TryGetFeatureValue(CommonUsages.menuButton, out bool menuButtonState);
+            if (menuButtonState)
+            {
+                ToggleCubeObject();
+            }
+        }
     }
+
+    private void Update()
+    {
+        // 매 프레임마다 메뉴 버튼의 상태를 체크하여 Cube 오브젝트를 토글합니다.
+        leftController.inputDevice.TryGetFeatureValue(CommonUsages.menuButton, out bool menuButtonState);
+        if (menuButtonState)
+        {
+            ToggleCubeObject();
+        }
+    }
+
+    private void ToggleCubeObject()
+    {
+        // Cube 오브젝트의 활성/비활성 상태를 변경합니다.
+        cubeObject.SetActive(!cubeObject.activeSelf);
+    }
+
 }
